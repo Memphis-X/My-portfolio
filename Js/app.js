da9ea599447b9db4440315c24d0d05daf2d31f00
Jordan <thememphiss@icloud.com>
2022-06-20 02:03:44 +0300

    few changes I dont remember

--- Js/app.js
+++ Js/app.js
@@ -251,38 +251,126 @@ particlesJS('particle-js',
   }
 );
 
-jQuery(function($){
-    $(window).scroll(function() {
-
-  // selectors
-  var $window = $(window),
-      $body = $('body'),
-      $panel = $('.panel');
-      $section2 = $('.section2');
-
-  // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop() + ($window.height() / 20);
-
-  $panel.each(function () {
-    var $this = $(this);
-
-    // if position is within range of this panel.
-    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-
-      // Remove all classes on body with color-
-      $body.removeClass(function (index, css) {
-        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-      });
-
-      // Add class of currently active div
-      $body.addClass('color-' + $(this).data('color'));
+particlesJS('particl-js', 
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "100%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
     }
-  });
+  }
+);
 
-}).scroll();
-});
+
 
 const scrollProgress = document.getElementById('scroll-progress');
   const height =
@@ -294,6 +382,12 @@ const scrollProgress = document.getElementById('scroll-progress');
   });
 
 
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
 
 var VanillaTilt = (function () {
 'use strict';
@@ -619,61 +713,61 @@ class VanillaTilt {
    * Appends the glare element (if glarePrerender equals false)
    * and sets the default style
    */
-  // prepareGlare() {
-  //   // If option pre-render is enabled we assume all html/css is present for an optimal glare effect.
-  //   if (!this.glarePrerender) {
-  //     // Create glare element
-  //     const jsTiltGlare = document.createElement("div");
-  //     jsTiltGlare.classList.add("js-tilt-glare");
+  prepareGlare() {
+    // If option pre-render is enabled we assume all html/css is present for an optimal glare effect.
+    if (!this.glarePrerender) {
+      // Create glare element
+      const jsTiltGlare = document.createElement("div");
+      jsTiltGlare.classList.add("js-tilt-glare");
 
-  //     const jsTiltGlareInner = document.createElement("div");
-  //     jsTiltGlareInner.classList.add("js-tilt-glare-inner");
+      const jsTiltGlareInner = document.createElement("div");
+      jsTiltGlareInner.classList.add("js-tilt-glare-inner");
 
-  //     jsTiltGlare.appendChild(jsTiltGlareInner);
-  //     this.element.appendChild(jsTiltGlare);
-  //   }
+      jsTiltGlare.appendChild(jsTiltGlareInner);
+      this.element.appendChild(jsTiltGlare);
+    }
 
-  //   this.glareElementWrapper = this.element.querySelector(".js-tilt-glare");
-  //   this.glareElement = this.element.querySelector(".js-tilt-glare-inner");
+    this.glareElementWrapper = this.element.querySelector(".js-tilt-glare");
+    this.glareElement = this.element.querySelector(".js-tilt-glare-inner");
 
-  //   if (this.glarePrerender) {
-  //     return;
-  //   }
+    if (this.glarePrerender) {
+      return;
+    }
 
-  //   Object.assign(this.glareElementWrapper.style, {
-  //     "position": "absolute",
-  //     "top": "0",
-  //     "left": "0",
-  //     "width": "100%",
-  //     "height": "100%",
-  //     "overflow": "hidden",
-  //     "pointer-events": "none"
-  //   });
+    Object.assign(this.glareElementWrapper.style, {
+      "position": "absolute",
+      "top": "0",
+      "left": "0",
+      "width": "100%",
+      "height": "100%",
+      "overflow": "hidden",
+      "pointer-events": "none"
+    });
 
-  //   Object.assign(this.glareElement.style, {
-  //     "position": "absolute",
-  //     "top": "50%",
-  //     "left": "50%",
-  //     "pointer-events": "none",
-  //     "background-image": `linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)`,
-  //     "transform": "rotate(180deg) translate(-50%, -50%)",
-  //     "transform-origin": "0% 0%",
-  //     "opacity": "0",
-  //   });
+    Object.assign(this.glareElement.style, {
+      "position": "absolute",
+      "top": "50%",
+      "left": "50%",
+      "pointer-events": "none",
+      "background-image": `linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)`,
+      "transform": "rotate(180deg) translate(-50%, -50%)",
+      "transform-origin": "0% 0%",
+      "opacity": "0",
+    });
 
-  //   this.updateGlareSize();
-  // }
+    this.updateGlareSize();
+  }
 
-  // updateGlareSize() {
-  //   if (this.glare) {
-  //     const glareSize = (this.element.offsetWidth > this.element.offsetHeight ? this.element.offsetWidth : this.element.offsetHeight) * 2;
+  updateGlareSize() {
+    if (this.glare) {
+      const glareSize = (this.element.offsetWidth > this.element.offsetHeight ? this.element.offsetWidth : this.element.offsetHeight) * 2;
 
-  //     Object.assign(this.glareElement.style, {
-  //       "width": `${glareSize}px`,
-  //       "height": `${glareSize}px`,
-  //     });
-  //   }
-  // }
+      Object.assign(this.glareElement.style, {
+        "width": `${glareSize}px`,
+        "height": `${glareSize}px`,
+      });
+    }
+  }
 
   updateClientSize() {
     this.clientWidth = window.innerWidth
@@ -807,11 +901,36 @@ return VanillaTilt;
 
 }());
 
+// jQuery(function($){
+//     $(window).scroll(function() {
 
+//   // selectors
+//   var $window = $(window),
+//       $body = $('body'),
+//       $panel = $('.panel');
+//       $section2 = $('.section2');
 
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
+//   // Change 33% earlier than scroll position so colour is there when you arrive.
+//   var scroll = $window.scrollTop() + ($window.height() / 20);
+
+//   $panel.each(function () {
+//     var $this = $(this);
+
+//     // if position is within range of this panel.
+//     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+//     // Remember we set the scroll to 33% earlier in scroll var.
+//     if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+//       // Remove all classes on body with color-
+//       $body.removeClass(function (index, css) {
+//         return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+//       });
+
+//       // Add class of currently active div
+//       $body.addClass('color-' + $(this).data('color'));
+//     }
+//   });
+
+// }).scroll();
+// });
+
